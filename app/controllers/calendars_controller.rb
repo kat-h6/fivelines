1d4b7bf8e5f6e18b6fae566a542377b5cc4a36be
Sheila Leveille <sbleveille@gmail.com>
2021-06-10 13:24:46 -0500

    FEAT: Added simple_calendar gem and calendar to the calendars route

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,14 @@
 class CalendarsController < ApplicationController
   def show
     @lessons = current_user.lessons
+
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    # For a monthly view:
+    @meetings = Lesson.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+
+    # # Or, for a weekly view:
+    # @meetings = Lesson.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
   end
 end
