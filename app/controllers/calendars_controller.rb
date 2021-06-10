4e09ef576c45d704603e95929ae59dfdc6b49a14
Katharine Hao <hao.katharine@gmail.com>
2021-06-10 21:06:47 +0300

    cleaning, calendar bug still

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,8 @@
 class CalendarsController < ApplicationController
   def show
-    @lessons = current_user.lessons
+    @user_lessons = current_user.lessons
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @lessons = Lesson.where(starts_at:
+      start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
