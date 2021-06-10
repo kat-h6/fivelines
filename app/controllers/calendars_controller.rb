561a4be67b42e3eb34eff35afbf359c6fb1b55c8
Katharine Hao <hao.katharine@gmail.com>
2021-06-10 21:26:52 +0300

    cleaning

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
   def show
     @user_lessons = current_user.lessons
     start_date = params.fetch(:start_date, Date.today).to_date
-    @lessons = Lesson.where(starts_at:
+    @lessons = Lesson.where(start_time:
       start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
