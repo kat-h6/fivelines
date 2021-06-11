fea7291dc067a6a698a05129e5d117cc2f1f5386
Sepideh S <75858394+Sapideh1071@users.noreply.github.com>
2021-06-11 21:10:48 +0200

    routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
       
     end
   end
+
+  resources :resources, only: [:new, :create, :index, :destroy]
     
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
