0b5f34464a6477a5ea824127e0f75c4770e64ce3
Christopher Haslerud <haslerud1@hotmail.com>
2021-06-12 13:00:54 +0200

    adding student list

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,7 @@ Rails.application.routes.draw do
 
   resources :assignments, only: [:show, :update]
 
-  resources :users do 
-    member do 
-      get :student_assignments
-      
-    end
-  end
-    
+  resources :students, only: :index
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
