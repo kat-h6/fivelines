ea4d63f0b0d3ce76b852176f6c5d3aaf08bb2e9f
Tavis David <tavis.david@airthings.com>
2021-06-15 23:49:07 +0200

    fixed homepage styling and added global navbar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,9 @@ class CalendarsController < ApplicationController
     # For a monthly view:
     @meetings = Lesson.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
 
+    # For a daily view:
+    @schedule = Lesson.where(start_time: start_date.beginning_of_month.beginning_of_week.beginning_of_day..start_date.end_of_month.end_of_week.end_of_day)
+
     # # Or, for a weekly view:
     # @meetings = Lesson.where(starts_at: start_date.beginning_of_week..start_date.end_of_week)
   end
